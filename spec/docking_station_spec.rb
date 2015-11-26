f8d2facbddc443dfe11e4ff8882ac25b00dc5bdc
allimac <camillacolser@hotmail.com>
2015-11-26 11:17:56 +0000

    finished 14th

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,12 +10,12 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
 
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes.last).to eq bike
   end
 
   describe '#release_bike' do
@@ -24,6 +24,7 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
     end
@@ -34,12 +35,13 @@ describe DockingStation do
 
     it 'docks something' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq subject.bikes
     end
 
     it 'raises an error when bike is already docked' do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error 'Docking Station is already full'
     end
+
   end
 end
