ee2f8a4d293a48f9611801aa677f048f60f3ada9
Chris Wynne <chris.wynndow@gmail.com>
2015-11-27 11:30:23 +0000

    Adds first user story for 18

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,9 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to :working }
+
+  it 'allows a bike to be reported broken' do
+    subject.report_broken
+    expect(subject.broken?).to eq true
+  end
 end
