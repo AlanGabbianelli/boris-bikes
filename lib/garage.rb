65b10a3b91adba1ee9570a288467da4e2f09fcb7
Chris Wynne <chris.wynndow@gmail.com>
2015-11-27 18:32:46 +0000

    Adds a small amount of code for emptying van

--- lib/garage.rb
+++ lib/garage.rb
@@ -4,6 +4,7 @@ class Garage
 
   def receive_bikes_from_van(van)
     @bikes_to_be_fixed = (van.bikes_on_board)
+
     @bikes_to_be_fixed
   end
 
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -12,4 +12,13 @@ describe Van do
     expect(subject.bikes_on_board).to eq [bike]
   end
 
+  it 'removes bikes from van once delivered' do
+    bike = double(:bike, broken?: true)
+    station = double(:station, separate_broken_bikes: [bike])
+    garage = double(:garage, receive_bikes_from_van: [] )
+    subject.collect_bikes(station)
+    garage.recieve_bikes_from_van(subject)
+    expect
+  end
+
 end
