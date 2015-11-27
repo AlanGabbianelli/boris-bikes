68a470d4a8b09378221c7aee0fa8b77c29a122f6
Chris Wynne <chris.wynndow@gmail.com>
2015-11-27 15:53:18 +0000

    Adds van.rb and feature test

---
+++ spec/feature_spec.rb
@@ -0,0 +1,18 @@
+require 'docking_station'
+require 'van'
+
+describe 'feature test' do
+
+  it ' van collects broken bikes from station' do
+    bike = Bike.new
+    station = DockingStation.new
+    van = Van.new
+    bike.report_broken
+    station.dock(bike)
+    van.collect_bikes(station)
+    expect(van.bikes_on_board).to eq [bike]
+
+  end
+
+
+end
